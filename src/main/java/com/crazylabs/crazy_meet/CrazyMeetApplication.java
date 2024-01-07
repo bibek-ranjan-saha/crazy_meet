328cdd4e5ef526a5ba830d4dde9edd348a303983
Bibek Saha <bibeksahabiki@gmail.com>
2024-01-07 22:33:21 +0530

    Initial commit

---
+++ src/main/java/com/crazylabs/crazy_meet/CrazyMeetApplication.java
@@ -0,0 +1,13 @@
+package com.crazylabs.crazy_meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crazylabs/crazy_meet/CrazyMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazylabs.crazy_meet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
